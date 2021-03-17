1829e748eec22d228c54156ea720afaca6ccc40a
Miguel Cebollero <miceboll@microsoft.com>
2021-03-17 14:26:45 -0400

    Add project files.

---
+++ DemoDatabase/SalesLT/Views/vGetAllCategories.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW [SalesLT].[vGetAllCategories]
+AS
+WITH   CategoryCTE ([ParentProductCategoryID], [ProductCategoryID], [Name])
+AS     (SELECT [ParentProductCategoryID],
+               [ProductCategoryID],
+               [Name]
+        FROM   SalesLT.ProductCategory
+        WHERE  ParentProductCategoryID IS NULL
+        UNION ALL
+        SELECT C.[ParentProductCategoryID],
+               C.[ProductCategoryID],
+               C.[Name]
+        FROM   SalesLT.ProductCategory AS C
+               INNER JOIN
+               CategoryCTE AS BC
+               ON BC.ProductCategoryID = C.ParentProductCategoryID)
+SELECT PC.[Name] AS [ParentProductCategoryName],
+       CCTE.[Name] AS [ProductCategoryName],
+       CCTE.[ProductCategoryID]
+FROM   CategoryCTE AS CCTE
+       INNER JOIN
+       SalesLT.ProductCategory AS PC
+       ON PC.[ProductCategoryID] = CCTE.[ParentProductCategoryID];
+
+
